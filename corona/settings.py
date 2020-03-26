47f31e60a18eb0b576d5608e2b930f032934087c
hyeonukdev <lego7407@gmail.com>
2020-03-26 18:54:30 +0900

    crontab

--- corona/settings.py
+++ corona/settings.py
@@ -14,6 +14,8 @@ import os
 import sys
 import django
 
+CRONTAB_DJANGO_SETTINGS_MODULE = 'corona.settings.local_settings'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
